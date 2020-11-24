c3f2c655ed46b5c43f66cb27d11d7c1fe6dd850c
Wrokbrook repo <chukschiboy@gmail.com>
2020-11-23 20:00:00 -0800

    update on the firebase deployment and updated Todos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Todo from './Todo';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
-import db from './src/firebase';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
 
@@ -12,19 +13,26 @@ function App() {
   //  'Take dogs for a walk', 'Take the garbage out', "Chidiebere has started taking massive and great actions today" (Initial temporary data before the database)
 
   // When the app loads, we need to listen to the database and fetch new Todos as they get added/removed
+
   useEffect( () => {
     // this code here... fires when the app.js loads
-    db.collection('todos').onSnapshot(snapshot => {
-      console.log(snapshot.docs.map(doc => doc.data().todo));
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
     })
   }, []);
 
   const addTodo = (event) => {
     // This will fire off when we click the button
     event.preventDefault(); // This will stop the REFRESH
-    console.log( '🚀', "I'm working!!!");
-    setTodos([...todos, input]);
+
+    // console.log(firebase.firestore.FieldValue.serverTimestamp());
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    // setTodos([...todos, input]);
     setInput(''); // clear up the input after submitting
   }
 
@@ -41,8 +49,7 @@ function App() {
 
       <ul>
         {todos.map(todo => (
-          <Todo text={todo} />
-          // <li>{todo}</li>
+          <Todo todo={todo} />
         ))}
       </ul>
     </div>
